18c27015ed8334efb98975aae5a69f62946ff089
Hogar Maldonado <Hogar Maldonado@DESKTOP-J643LCQ>
2021-05-30 20:44:14 +0200

    fixing flake 8/ pep8 error

--- calculator.py
+++ calculator.py
@@ -15,10 +15,9 @@ def multiply(first_term, second_term):
     return first_term * second_term
 
 
-def calc_pow(first_term, second_term: int):  # Switched to calc_pow to avoid confusion with math.pow.
-
-    # TIP: Adding <arg_name> : <data_type> applies type check over argument (Ex. second_term: int )
-
+# Switched to calc_pow to avoid confusion with math.pow
+# You can enforce arg type check with <arg_name> : <data_type>
+def calc_pow(first_term, second_term: int):
     return first_term ** second_term
 
 
