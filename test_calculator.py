bd15111e3ebe16a60a0924890225021d30457e30
dfu5823 <71274065+dfu5823@users.noreply.github.com>
2021-05-31 08:37:57 -0500

    Add an integration test class

--- test_calculator.py
+++ test_calculator.py
@@ -21,3 +21,25 @@ class TestCalculator:
 
     def test_pow(self):
         assert 8 == calculator.calc_pow(2, 3)
+
+
+class TestCalculatorData:
+    def setUp(self):
+        # Load data for any test case (pair of numbers)
+        self.data = [4, 2]
+
+    def test_addition(self):
+        assert self.data(1) + self.data(2) == \
+               calculator.add(self.data(1), self.data(2))
+
+    def test_subtraction(self):
+        assert 2 == calculator.subtract(4, 2)
+
+    def test_multiplication(self):
+        assert 100 == calculator.multiply(10, 10)
+
+    def test_spit_float(self):
+        assert 1.0 == calculator.spit_float()
+
+    def test_pow(self):
+        assert 8 == calculator.calc_pow(2, 3)
