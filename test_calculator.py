a77cf0cfea79b4731d4fb5a8e2ec6f20b23cb24f
dfu5823 <71274065+dfu5823@users.noreply.github.com>
2021-05-31 08:48:12 -0500

    Finish Integration test

--- test_calculator.py
+++ test_calculator.py
@@ -30,17 +30,23 @@ class TestCalculatorData:
 
     def test_addition(self):
         self.data = [4, 2]
-        assert self.data[1] + self.data[2] == \
-               calculator.add(self.data[1], self.data[2])
+        assert self.data[0] + self.data[1] == \
+               calculator.add(self.data[0], self.data[1])
 
     def test_subtraction(self):
-        assert 2 == calculator.subtract(4, 2)
+        self.data = [4, 2]
+        assert self.data[0] - self.data[1] == \
+               calculator.subtract(self.data[0], self.data[1])
 
     def test_multiplication(self):
-        assert 100 == calculator.multiply(10, 10)
+        self.data = [4, 2]
+        assert self.data[0] * self.data[1] == \
+               calculator.multiply(self.data[0], self.data[1])
 
     def test_spit_float(self):
         assert 1.0 == calculator.spit_float()
 
     def test_pow(self):
-        assert 8 == calculator.calc_pow(2, 3)
+        self.data = [4, 2]
+        assert self.data[0] ** self.data[1] == \
+               calculator.pow(self.data[0], self.data[1])
