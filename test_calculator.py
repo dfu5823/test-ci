923c867b498bf9e883979d406436f9e8b1e129da
Juan Maldonado <maldonadojsm@gmail.com>
2021-05-31 22:51:10 +0200

    hopefully flake8 doesn't freak out lol

--- test_calculator.py
+++ test_calculator.py
@@ -7,57 +7,54 @@ import calculator
 
 class TestCalculator:
 
-    def test_addition(self):
-        assert 4 == calculator.add(2, 2)
+	def test_addition(self):
+		assert 4 == calculator.add(2, 2)
 
-    def test_subtraction(self):
-        assert 2 == calculator.subtract(4, 2)
+	def test_subtraction(self):
+		assert 2 == calculator.subtract(4, 2)
 
-    def test_multiplication(self):
-        assert 100 == calculator.multiply(10, 10)
+	def test_multiplication(self):
+		assert 100 == calculator.multiply(10, 10)
 
-    def test_spit_float(self):
-        assert 1.0 == calculator.spit_float()
+	def test_spit_float(self):
+		assert 1.0 == calculator.spit_float()
 
-    def test_pow(self):
-        assert 8 == calculator.calc_pow(2, 3)
-        
-    def test_integration(self):
-        # Confirm that 2 + 1 returns 3
-        val = calculator.add(2,1)
-        assert 3 == val
-        
-        # Confirm that calc_pow(2,val) returns 8
-        assert 8 == calculator.calc_pow(2,val)
-        
-        
-        
+	def test_pow(self):
+		assert 8 == calculator.calc_pow(2, 3)
+
+	def test_integration(self):
+		# Confirm that 2 + 1 returns 3
+		val = calculator.add(2, 1)
+		assert 3 == val
+
+		# Confirm that calc_pow(2,val) returns 8
+		assert 8 == calculator.calc_pow(2, val)
 
 
 class TestCalculatorData:
-    def test_setUp(self):
-        # Load data for any test case (pair of numbers)
-        self.data = [4, 2]
+	def test_setUp(self):
+		# Load data for any test case (pair of numbers)
+		self.data = [4, 2]
 
-    def test_addition(self):
-        self.data = [4, 2]
-        assert self.data[0] + self.data[1] == \
-               calculator.add(self.data[0], self.data[1])
+	def test_addition(self):
+		self.data = [4, 2]
+		assert self.data[0] + self.data[1] == \
+			   calculator.add(self.data[0], self.data[1])
 
-    def test_subtraction(self):
-        self.data = [4, 2]
-        assert self.data[0] - self.data[1] == \
-               calculator.subtract(self.data[0], self.data[1])
+	def test_subtraction(self):
+		self.data = [4, 2]
+		assert self.data[0] - self.data[1] == \
+			   calculator.subtract(self.data[0], self.data[1])
 
-    def test_multiplication(self):
-        self.data = [4, 2]
-        assert self.data[0] * self.data[1] == \
-               calculator.multiply(self.data[0], self.data[1])
+	def test_multiplication(self):
+		self.data = [4, 2]
+		assert self.data[0] * self.data[1] == \
+			   calculator.multiply(self.data[0], self.data[1])
 
-    def test_spit_float(self):
-        assert 1.0 == calculator.spit_float()
+	def test_spit_float(self):
+		assert 1.0 == calculator.spit_float()
 
-    def test_pow(self):
-        self.data = [4, 2]
-        assert self.data[0] ** self.data[1] == \
-               calculator.calc_pow(self.data[0], self.data[1])
+	def test_pow(self):
+		self.data = [4, 2]
+		assert self.data[0] ** self.data[1] == \
+			   calculator.calc_pow(self.data[0], self.data[1])
